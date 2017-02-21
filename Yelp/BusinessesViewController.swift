3daf44fe10c77d3dd3e61df4e8f0ec6d21bdec2a
Alex Sumak <alex.sumak@Alex-Sumaks-MacBook-Air.local>
2017-02-20 21:08:59 -0800

    op

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -12,30 +12,28 @@ class BusinessesViewController: UIViewController{
     
     //infin scroll vars
     var isMoreDataLoading = false
-    var loadMore = 20
     var searcher: String = ""
-    let userDefaults = UserDefaults.standard
     
     //searchbar var
     @IBOutlet weak var SearchBar: UISearchBar!
     
-    var businesses: [Business]!
+    lazy var businesses = [Business]() //initially, this is empty array of business
+    //var businesses: [Business]!  this is an optional
     
     //table view var
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
         //initilazers for UITableViewDataSource, UITableViewDelegate
         tableView.delegate = self
         tableView.dataSource = self
-     
+        
         // for constraints
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 120
-
-
+        
+        
         // search bar things
         self.SearchBar.delegate = self
         self.SearchBar.barTintColor = UIColor.clear
@@ -44,27 +42,29 @@ class BusinessesViewController: UIViewController{
         self.navigationItem.titleView = self.SearchBar
         self.SearchBar.tintColor = UIColor.black
         
-        
-       
-        Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
-            
-            self.businesses = businesses
-            self.tableView.reloadData()
-            if let businesses = businesses {
-                for business in businesses {
-                    print(business.name!)
-                    print(business.address!)
-                }
-            }
-            
-            }
-        )
+        //before this point, because the lazy, the business has not been initialized
+        //and it will be initilaized only when somewhere in your code start using it
+        self.loadData()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func loadData() {
+        self.isMoreDataLoading = true
+        Business.searchWithTerm(term: "Thai", offset: self.businesses.count, completion: { (businesses: [Business]?, error: Error?) -> Void in
+            if let businesses = businesses{
+                //if there is a chance that this variable will be nil, then you want to use if let statement
+                //this won't crash your app when it's nil
+                self.businesses.append(contentsOf: businesses) //self.businesses will contain 20 buesiness, for the first fetch
+                self.isMoreDataLoading = false
+                self.tableView.reloadData()
+            }
+        })
+    }
+    
 }
 
 
@@ -73,18 +73,14 @@ extension BusinessesViewController: UITableViewDataSource, UITableViewDelegate{
     
     // requared function for UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if businesses != nil {
-            return self.businesses!.count
-        } else {
-            return 0
-        }
+        return self.businesses.count
     }
     
     
     // requared function for UITableViewDataSource
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier:"BusinessCell" , for: indexPath) as! BusinessCell
-        cell.business = businesses![indexPath.row]
+        cell.business = businesses[indexPath.row]
         
         return cell
     }
@@ -116,14 +112,9 @@ extension BusinessesViewController: UISearchBarDelegate{
         searchBar.endEditing(true)
         let searchText = searchBar.text!
         Business.searchWithTerm(term: searchText, completion: { (businesses: [Business]?, error: Error?) -> Void in
-            
-            self.businesses = businesses
-            self.tableView.reloadData()
-            if let businesses = businesses {
-                for business in businesses {
-                    print(business.name!)
-                    print(business.address!)
-                }
+            if let businesses = businesses{
+                self.businesses = businesses
+                self.tableView.reloadData()
             }
         }
         )
@@ -144,29 +135,8 @@ extension BusinessesViewController: UIScrollViewDelegate{
             
             // When the user has scrolled past the threshold, start requesting
             if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.isDragging) {
-                isMoreDataLoading = true
-                
-                self.loadMore += 20
-                
-                loadMoreData()
-                
-                self.tableView.reloadData()
+                self.loadData()
             }
         }
-    }
-    
-    func loadMoreData() {
-        let prev = self.userDefaults.string(forKey: "searchQuery")
-        
-        Business.searchWithTerm(term: prev!, offset: loadMore, completion: { (businesses: [Business]?, error: Error?) -> Void in
-            
-            self.businesses.append(contentsOf: businesses!)
-            
-            self.isMoreDataLoading = false
-            
-            self.tableView.reloadData()
-            
-        })
-    
     }
 }
